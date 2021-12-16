f2ea6d2c2c892630672d227d213a39a97d75755c
Omer Aqeel <93266569+OmerAqeel@users.noreply.github.com>
2021-12-16 20:30:44 +0000

    Add files via upload

---
+++ display.java
@@ -0,0 +1,26 @@
+
+public class display {
+		public static void GameRules() {
+			System.out.println(
+					"*======================================================GAME RULES======================================================================*");
+			System.out.println("");
+			System.out.println("O    You are supposed to guess the letter in each word that is presented  ");
+			System.out.println("");
+			System.out.println("O    You will be given 10 guesses to guess the secret word");
+			System.out.println("");
+			System.out.println("O    If you are a NEWBIE, then your guesses will reduce by 1 at every wrong guess ");
+			System.out.println("");
+			System.out.println("       o     If you are an AMATUER , then your guesses will reduce by 2 at every wrong guess");
+			System.out.println("");
+			System.out.println("                o     If you are a LEGEND , then your guesses will reduce by 3 at every wrong guess");
+			System.out.println("");
+			System.out.println("O    You are not supposed to enter any invalid character i.e. numbers/special characters");
+			System.out.println("");
+			System.out.println("O    You will recieve warning at every invalid entery, if you recieve 3 warnings your guesses will start deducting by 1 ");
+			System.out.println("");
+			System.out.println("O    To end the game just enter END into the console");
+			System.out.println("");
+			System.out.println(
+					"*======================================================GAME RULES======================================================================*");
+		}
+}
